51fe35ebbb91ec60044bd981ea532bf5fa18af61
zhangsan <zhangsan@163.com>
2019-02-18 17:02:22 +0800

    添加git-test2到git

---
+++ git-test2/src/com/ms/git/Demo01.java
@@ -0,0 +1,9 @@
+package com.ms.git;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hai git");
+        System.out.println("hai git");
+        System.out.println("hai git");
+    }
+}
