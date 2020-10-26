88e00ad720f9e54e0eae2aed920da33bed10cf58
zhangsan <zhangsan@163.com>
2020-10-26 19:51:17 +0800

    添加

---
+++ git-test2/src/com/ms/git/Demo02.java
@@ -0,0 +1,15 @@
+package com.ms.git;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hai git");
+     
+        //hello test
+    }
+    public void test3(){
+        int i  = 0;
+        i+=10;
+        System.out.println(i);
+       
+    }
+}
