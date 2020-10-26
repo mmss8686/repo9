b2def1894ef8fb0627f41f7937786cbeb5b6247a
zhangsan <zhangsan@163.com>
2020-10-26 20:41:45 +0800

    gg

---
+++ git-test-project/src/com/ms/git/Demo002.java
@@ -0,0 +1,9 @@
+package com.ms.git;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
